7b1a3848d587f85ddf60764cd2a281ba6cac1633
Doron Chen <cdoron@il.ibm.com>
2022-08-30 18:12:44 +0300

    add openmetadata_connector_test_helper.go
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- openmetadata-connector-core/openmetadata_connector_test.go
+++ openmetadata-connector-core/openmetadata_connector_test.go
@@ -1,105 +1,14 @@
 package openapiconnectorcore
 
 import (
-	"encoding/json"
-	"errors"
-	"io"
 	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
 	"os"
 	"testing"
 
 	"fybrik.io/fybrik/pkg/logging"
-	"github.com/rs/zerolog"
 
 	"fybrik.io/openmetadata-connector/vault"
 )
-
-const Token = "vaultToken"
-const JWT = "myJWT"
-const PathInVault = "/v1/kubernetes/info?namespace=default"
-const AccessKey = "myAccessKey"
-const SecretKey = "mySecretKey"
-
-func mustAsJSON(t *testing.T, in interface{}) []byte {
-	result, err := json.Marshal(in)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return result
-}
-
-func createMockVaultServer(t *testing.T) *httptest.Server {
-	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestBytes, err := io.ReadAll(r.Body)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-
-		if r.Method == "POST" && r.RequestURI == "/v1/auth/kubernetes/login" {
-			requestMap := make(map[string]interface{})
-			err = json.Unmarshal(requestBytes, &requestMap)
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			if (requestMap["role"].(string) != "fybrik") ||
-				(requestMap["jwt"].(string) != JWT) {
-				t.Error(errors.New("Unexpected request format"))
-				return
-			}
-
-			response := map[string]interface{}{
-				"request_id": "00000000-0000-0000-0000-000000000000",
-				"auth": map[string]interface{}{
-					"client_token": Token,
-				},
-			}
-
-			responseBytes := mustAsJSON(t, response)
-
-			w.Header().Add("Content-Type", "application/json")
-			_, err = w.Write(responseBytes)
-			if err != nil {
-				t.Error(err)
-			}
-
-			return
-		}
-
-		if r.Method == "GET" {
-			if r.Header.Get("X-Vault-Token") != Token {
-				t.Error(err)
-			}
-			logger.Trace().Msg("About to mock response secret request")
-			response := map[string]interface{}{
-				"data": map[string]interface{}{
-					"access_key": AccessKey,
-					"secret_key": SecretKey,
-				},
-			}
-
-			responseBytes := mustAsJSON(t, response)
-
-			w.Header().Add("Content-Type", "application/json")
-			_, err = w.Write(responseBytes)
-			if err != nil {
-				t.Error(err)
-			}
-
-			return
-		}
-		t.Error(errors.New(""))
-	}))
-
-	return svr
-}
-
-var logger zerolog.Logger
-var jwtFile *os.File
-var mockVaultServer *httptest.Server
 
 func setupSuite(t *testing.T) func() {
 	logger = logging.LogInit(logging.CONNECTOR, "OpenMetadata Connector Tests")
