03503d42fc356fb50ee7d1ecaf56783dc78c282e
Doron Chen <cdoron@il.ibm.com>
2022-10-12 09:36:49 +0300

    add the prepare command
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- main.go
+++ main.go
@@ -74,6 +74,52 @@ func RunCmd() *cobra.Command {
 	return cmd
 }
 
+// PrepareCmd defines the command for preparing OM for Fybrik
+// by creating Fybrik tags and custom properties
+func PrepareCmd() *cobra.Command {
+	logger := logging.LogInit(logging.CONNECTOR, "Preparing OpenMetadata for Fybrik")
+	configFile := DefaultConfigFile
+	customizationFile := DefaultCustomizationFile
+	cmd := &cobra.Command{
+		Use:   "prepare",
+		Short: "Prepare OpenMetadata for Fybrik",
+		Run: func(cmd *cobra.Command, args []string) {
+			configFileBytes, err := os.ReadFile(configFile)
+			if err != nil {
+				logger.Error().Err(err).Msg("failure to read config file")
+				return
+			}
+
+			customizationFileBytes, err := os.ReadFile(customizationFile)
+			if err != nil {
+				logger.Error().Err(err).Msg("failure to read customization file")
+				return
+			}
+
+			conf := make(map[string]interface{})
+			customization := make(map[string]interface{})
+
+			err = yaml.Unmarshal(configFileBytes, &conf)
+			if err != nil {
+				logger.Error().Err(err).Msg("failure to parse config file")
+				return
+			}
+			err = yaml.Unmarshal(customizationFileBytes, &customization)
+			if err != nil {
+				logger.Error().Err(err).Msg("failure to parse customization file")
+				return
+			}
+
+			_ = occ.NewOpenMetadataAPIService(conf, customization, &logger)
+		},
+	}
+	cmd.Flags().StringVar(&configFile, "config", configFile, "Configuration file")
+	cmd.Flags().StringVar(&customizationFile, "customization", customizationFile,
+		"File containing tags and custom properties needed for working with Fybrik")
+	cmd.CompletionOptions.DisableDefaultCmd = true
+	return cmd
+}
+
 // RootCmd defines the root cli command
 func RootCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -81,6 +127,7 @@ func RootCmd() *cobra.Command {
 		Short: "Kubernetes based OpenMetadata data catalog connector for Fybrik",
 	}
 	cmd.AddCommand(RunCmd())
+	cmd.AddCommand(PrepareCmd())
 	return cmd
 }
 
